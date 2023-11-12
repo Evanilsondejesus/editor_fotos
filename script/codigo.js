62b479950f3b173041bd6787a3b4d6ed38728433
Evanilson <88106712+Evanilsondejesus@users.noreply.github.com>
2023-11-12 09:41:18 -0300

    Update codigo.js

--- script/codigo.js
+++ script/codigo.js
@@ -5,13 +5,11 @@ const div =  document.querySelector("#insirir_img");
 const range =  document.querySelector("input[type=range]");
 
 
-
 var valores = { Brilho: { value: 50, max: 200 },  Cor: { value: 40, max: 200 },  Inversao: { value: 0, max: 100 },}
 
 let filtroativo;
 button[0].classList.add("tons")
 valores["Brilho"].value = 150
-
 
 
 const novofoto = ()=>{
@@ -21,100 +19,44 @@ input.click();
 
 
 input.onchange =()=>{
-
-
-
-  let file = input.files[0];
-  img.src = URL.createObjectURL(file);
-
- 
-
+let file = input.files[0];
+img.src = URL.createObjectURL(file);
+}
 
 }
 
- 
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
- 
 
 
 button.forEach((item) => {
- 
-
 
 item.onclick = ()=>{
-  document.querySelector(".tons").classList.remove("tons");
- 
- item.classList.add("tons")
- 
+document.querySelector(".tons").classList.remove("tons");
+item.classList.add("tons")
 filtroativo = item.innerText;
-
-
 range.value = valores[item.innerText].value;
-
-
-
-
 
 }
 
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//criar canvas para poder abaixa a foto
 function Abaixar() {
    
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
   canvas.width = img.naturalWidth;
   canvas.height = img.naturalHeight;
- 
-  ctx.filter = `
-    brightness(${valores["Brilho"].value}%) 
-    grayscale(${valores["Cor"].value}%) 
-    invert(${valores["Inversao"].value}%)
+  
+ ctx.filter = `
+ brightness(${valores["Brilho"].value}%) 
+ grayscale(${valores["Cor"].value}%) 
+ invert(${valores["Inversao"].value}%)
   `;
 
 
 
-
- 
-
-
-
-  ctx.translate(canvas.width / 2, canvas.height / 2);
+ctx.translate(canvas.width / 2, canvas.height / 2);
   ctx.drawImage(
     img,
     -canvas.width / 2,
@@ -128,55 +70,12 @@ function Abaixar() {
   link.href = canvas.toDataURL();
   link.click();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
 
-
-
-
-
 function Auterar(v) {
-  
- 
-
- valores[filtroativo].value = v.value
+valores[filtroativo].value = v.value
 
  
   img.style.filter = `
